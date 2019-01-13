2d2733250e54d9daef9e59dce4e9ba0e669c9ca9
pompia <45210867+pompia@users.noreply.github.com>
2019-01-13 21:01:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,50 +2,47 @@ const Discord = require("discord.js");
   const kboosh = new Discord.Client();
     const kbooshtoken = "NTM0MDgzMjI3OTYzMzU5MjQx.Dx0nvw.Duw1w44ZaaypCNTnX_7gWsLCCyY"
       kboosh.on('ready', () => {
-        kboosh.user.setGame(MAX NETWORK Broadcast,'https://www.twitch.tv/MAX-Broadcast%27);
+        kboosh.user.setGame(`MAX Broadcast`,'https://www.twitch.tv/MAX-Broadcast');
           console.log('Im Ready!');
-
+  
         });
 
   kboosh.on('message', message => {
-    if (message.content.split(' ')[0] == '#bc')
+    if (message.content.split(' ')[0] == '%k')
        message.guild.members.forEach( member => {
          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 
 
-           member.send( ${member} ! + "" + message.guild.name + " :  " + message.content.substr(3));
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
                                                       message.delete();
-
+            
                                                     });
-
+            
                                                   });
    kboosh.on("message", message => {
-       var prefix = "#";
+       var prefix = "%";
  
              var args = message.content.substring(prefix.length).split(" ");
                 if (message.content.startsWith(prefix + "b")) {
                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-Twitch
-Twitch
-Twitch is the world's leading video platform and community for gamers.
 
-if (!args[1]) {
-
+                          if (!args[1]) {
+                            
                                  let embed3 = new Discord.RichEmbed()
                                      .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
                                        .setColor("#FF00FF")
                                           message.channel.sendEmbed(embed3);
-
+                            
                                         } else {
 
-
+                            
                                            let embed4 = new Discord.RichEmbed()
                                                             .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
                                                                 .setColor("#99999")
-
+                               
                                                                 message.channel.sendEmbed(embed4);
                                                       message.delete();
                             }
                           }
 });
-kboosh.login(kbooshtoken);يها شيء
+kboosh.login(kbooshtoken);
